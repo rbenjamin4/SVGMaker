62535a1f0ccfeeb2fecc8ed3d2bbad2c8350fa65
Reed <reedbenjamin4@gmail.com>
2024-02-08 14:42:14 -0500

    Added generateLogo and writeFile functions, check for error, console log generated logo message, updated shape classes

--- index.js
+++ index.js
@@ -27,6 +27,40 @@ const input = await inquirer.prompt([
         message: 'Please enter the color you would like to use for the shape (using color name or hexadecimal number',
       }
   ])
+  .then((data) => {
+    const generateLogo = (data) => {
+        let shapeClass
+        let text = data.text
+        let textColor = data.textColor
+        let shape = data.shape
+        let shapeColor = data.shapeColor
+
+        if (shape === 'triangle'){
+            shapeClass = new Triangle()
+        } else if (shape === 'square'){
+            shapeClass = new Square()
+        } else if (shape === 'circle'){
+            shapeClass = new Circle()
+        }
+        shapeClass.setColor(shapeColor)
+
+    return `<svg version="1.1" width="300" height="200">${shapeClass.render()}
+    <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
+    </svg>`
+    }
+
+    const SVG = generateLogo(data)
+
+    fs.writeFile('./examples/logo.svg', SVG, function(err){
+      if(err){
+      throw err
+      }else{
+          console.log('Generated logo.svg')
+      }
+  })
+    
+  })
+
 
 }
   
