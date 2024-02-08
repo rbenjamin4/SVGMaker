46b7c6d945c6a99af305f6350c8fcdbea041a5ed
Reed <reedbenjamin4@gmail.com>
2024-02-08 17:11:51 -0500

    Added XML link from W3 in generateLogo function so SVG file can be viewed correctly in browser

--- index.js
+++ index.js
@@ -44,7 +44,7 @@ const input = await inquirer.prompt([
         }
         shapeClass.setColor(shapeColor)
 
-    return `<svg version="1.1" width="300" height="200">${shapeClass.render()}
+    return `<svg version="1.1" width="300" height="200" xmlns="http://www.w3.org/2000/svg">${shapeClass.render()}
     <text x="150" y="125" font-size="60" text-anchor="middle" fill="${textColor}">${text}</text>
     </svg>`
     }
